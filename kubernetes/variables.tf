273ea347bf1b0d02537a91d5c80e0a3bd8203552
kigawa <mail@kigawa.net>
2025-10-25 13:48:48 +0900

    ろいろいい

--- kubernetes/variables.tf
+++ kubernetes/variables.tf
@@ -1,23 +1,5 @@
-variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "prod"
-}
-
 variable "kubernetes_config_path" {
   description = "Path to the Kubernetes config file"
   type        = string
   default     = "~/.kube/config"
-}
-
-variable "apply_one_dev_manifests" {
-  description = "Whether to apply the one-dev manifests"
-  type        = bool
-  default     = false
-}
-
-variable "proxy_namespace" {
-  description = "The namespace for the proxy resources"
-  type        = string
-  default     = "infra"
 }
