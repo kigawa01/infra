41c872cbe1e045f8cf8f34aef5273800b1ef2bf4
kigawa <mail@kigawa.net>
2025-10-26 17:21:24 +0900

    wp

---
+++ kubernetes/providers.tf
@@ -0,0 +1,22 @@
+
+module "shared" {
+  source = "../shared"
+}
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+
+}
+
+# Configure the Kubernetes provider
+provider "kubernetes" {
+}
---
+++ variables.tf
@@ -0,0 +1,59 @@
+variable "bucket" {
+  description = "Generated variable: bucket"
+  type        = string
+  default     = ""
+}
+
+variable "endpoint" {
+  description = "Generated variable: endpoint"
+  type        = string
+  default     = ""
+}
+
+variable "access_key" {
+  description = "Generated variable: access_key"
+  type        = string
+  default     = ""
+}
+
+variable "secret_key" {
+  description = "Generated variable: secret_key"
+  type        = string
+  default     = ""
+}
+
+variable "ssh_key" {
+  description = "Generated variable: ssh_key"
+  type        = string
+  default     = ""
+}
+
+variable "bucket" {
+  description = "Generated variable: bucket"
+  type        = string
+  default     = ""
+}
+
+variable "endpoint" {
+  description = "Generated variable: endpoint"
+  type        = string
+  default     = ""
+}
+
+variable "access_key" {
+  description = "Generated variable: access_key"
+  type        = string
+  default     = ""
+}
+
+variable "secret_key" {
+  description = "Generated variable: secret_key"
+  type        = string
+  default     = ""
+}
+
+variable "ssh_key" {
+  description = "Generated variable: ssh_key"
+  type        = string
+  default     = ""
+}
